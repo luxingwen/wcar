60dc31be42dfb3519fd82bc9788db1ed533902a7
dropliu <571439942@qq.com>
2020-01-07 15:30:44 +0800

    add pages desc and line

---
+++ pages/desc/desc.js
@@ -0,0 +1,71 @@
+// pages/desc/desc.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    desc: {
+      img: "http://b1-q.mafengwo.net/s14/M00/60/4A/wKgE2l1s62OAeuJjAAdnbTVTNWk290.JPG",
+      title: "(太阳)热情似火——来自北纬18度的问候",
+      content: "从西海岸向东海岸出发！碧海蓝天、 白沙 滩、热情骄阳都是海岛的特殊代名词，第一次去 三亚 度假，也一定要享受一次慵懒的沙滩日光浴，每天的景点安排一定不要超过2个，下午回房间稍作休息后，还可以去海滩边漫步 看夕阳、吹海风、听浪声，一扫各种疲倦和不开心。从西向东的四大海湾分别为三亚湾 、 大东海 、 亚龙湾 和后开发的 海棠湾 。"
+    }
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/line/line.js
@@ -0,0 +1,82 @@
+// pages/line/line.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    line:[{
+      img:"http://b1-q.mafengwo.net/s10/M00/69/89/wKgBZ1lZuz2AR_JMAAWdQrnZkYw92.jpeg",
+      title: "昆明西双版纳6日豪华三飞游",
+      price:1980
+        }, {
+        img: "http://b1-q.mafengwo.net/s15/M00/F1/D5/CoUBGV25PEKAHE8OAAW42Z34oyc97.jpeg ",
+      title: "昆明大理丽江6天5晚奢华游",
+      price: 1800
+      }, {
+        img: "http://b1-q.mafengwo.net/s15/M00/E3/A6/CoUBGV3nDXOAXFXOAE9JfOjLK7I676.gif",
+        title: "直飞三亚6日游",
+        price: 3400
+      }, {
+        img: "http://n1-q.mafengwo.net/s12/M00/31/0C/wKgED1wA7s6AdTELAAH4SrJN_go15.jpeg",
+        title: "哈尔滨雪乡5天4晚跟团游",
+        price: 2850
+      }]
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
